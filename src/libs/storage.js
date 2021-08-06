d19723a80a54b04796fa49746fa2c3b47806bf33
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-08-06 03:42:12 -0600

    Storage updated for user information

--- src/libs/storage.js
+++ src/libs/storage.js
@@ -2,52 +2,62 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 class Storage {
     static instance = new Storage();
-    
+
     store = async (key, value) => {
-        try{
+        try {
             await AsyncStorage.setItem(key, value);
             return true;
-        } catch (err){
+        } catch (err) {
             console.log('Storage store err', err);
             return false;
-        }
-    };
-
-    get = async key => {
-        try{
-            return await AsyncStorage.getItem(key);
-        } catch (err) {
-            console.log('Storage get error', err);
-            throw Error(err);
-        }
-    };
-
-    multiGet = async keys => {
-        try{
-            return await AsyncStorage.multiGet(keys);
-        } catch (err) {
-            console.log('Storage multiget error', err);
-            throw Error(err);
-        }
-    };
-
-    getAllKeys = async () => {
-        try{
-            return await AsyncStorage.getAllKeys();
-        } catch (err) {
-            console.log('Storage get all keys error', err);
-            throw Error(err);
-        }
-    };
-
-    remove =async key => {
-        try{
-            await AsyncStorage.removeItem(key);
-        } catch (err) {
-            console.log('Storage delte error', err);
-            throw Error(err);
-        }
     }
+};
+
+get = async key => {
+    try {
+        return await AsyncStorage.getItem(key);
+    } catch (err) {
+        console.log('Storage get err', err)
+        throw Error(err);
+    }
+};
+
+multiGet = async keys => {
+    try {
+        return await AsyncStorage.multiGet(keys);
+    } catch (err) {
+        console.log('Storage get err', err);
+        throw Error(err);
+    }
+};
+
+multiRemove = async keys => {
+    try {
+        await AsyncStorage.multiRemove(keys);
+        return true;
+    } catch (err) {
+        console.log('Multi remove err', err);
+        return false;
+    }
+};
+
+getAllKeys = async () => {
+    try {
+        return await AsyncStorage.getAllKeys();
+    } catch (err) {
+        console.log('Storage get all keys err', err)
+        throw Error(err);
+    }
+};
+
+remove = async key => {
+    try {
+        return await AsyncStorage.removeItem(key);
+    } catch (err) {
+        console.log('Storage get err', err)
+        throw Error(err);
+    }
+};
 }
 
 export default Storage;
