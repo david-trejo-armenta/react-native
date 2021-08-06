3cd58f30c0f618d0b74bf70a83dbed8e0ee2ec21
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-08-06 03:47:56 -0600

    User stack working

---
+++ src/components/UsersScreen/UserStack.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import Profile from './Profile'
+import ProfileEdit from './ProfileEdit'
+import Colors from '../../res/Colors'
+
+const Stack = createStackNavigator();
+
+const UserStack = () => {
+   return( 
+   <Stack.Navigator
+    screenOptions={{
+        headerShown: false,
+        headerStyle: {
+            backgroundColor: Colors.blackPearl,
+            shadowColor: Colors.blackPearl
+        },
+        headerTintColor: Colors.white,
+    }}>
+
+      <Stack.Screen
+        name="Profile"
+        component={Profile}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="ProfileEdit"
+        component={ProfileEdit}
+        options={{headerShown: false}}
+      />
+
+    </Stack.Navigator>
+    )
+};
+export default UserStack;
