f001795a52fd5f7f302a950a54c2a0a38c37f159
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-08-06 03:47:14 -0600

    Profile edit not working, still pending

---
+++ src/components/UsersScreen/ProfileEdit.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import  {View, Text} from 'react-native'
+
+class ProfileEdit extends React.Component{
+    render() {
+        return(
+            <View>
+                <Text>ProfileEdit</Text>
+            </View>
+        )
+    }
+}
+
+export default ProfileEdit;
