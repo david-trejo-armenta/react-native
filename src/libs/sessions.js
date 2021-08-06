2f6b6e64e06e1a5b1f0e28a852f6514a10ee0e0a
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-08-06 03:43:29 -0600

    Session added, resources for the Django API info

---
+++ src/libs/sessions.js
@@ -0,0 +1,107 @@
+import Storage from './storage'
+
+class UserSession {
+    static instance = new UserSession();
+
+    login = async body => {
+        try {
+            let request = await fetch(`https://django-api-ex.herokuapp.com/users/login/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body),
+            });
+            let response = await request.json();
+            try {
+                let key = `token-${response.user.username}`;
+                await Storage.instance.store(key, response.token);
+                key = `id-${response.user.username}`;
+                await Storage.instance.store(key, JSON.stringify(response.user));
+                return true;
+            } catch (err) {
+                return response;
+            }
+
+        } catch (err) {
+            console.log('Login error', err);
+            throw Error(err);
+        }
+    };
+
+    logout = async () => {
+        try {
+            const allkeys = await Storage.instance.getAllKeys();
+            const tokens = allkeys.filter(key => key.includes('token-'));
+            await Storage.instance.multiRemove(tokens);
+            const ids = allkeys.filter(key => key.includes('id-'));
+            await Storage.instance.multiRemove(ids);
+            return true;
+        } catch (err) {
+            console.log('Logout err', err);
+            return false;
+        }
+    };
+
+    signup = async body => {
+        try {
+            let request = await fetch(`https://django-api-ex.herokuapp.com/users/signup/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                },
+                body: JSON.stringify(body),
+            });
+            let response = await request.json();
+            if (typeof response.username == 'string') {
+                return response.username;
+            } else {
+                return response;
+            }
+        } catch (err) {
+            console.log('Sing up Err', err);
+            throw Error(err);
+        }
+    };
+
+    getUser = async () => {
+        try {
+            const allKeys = await Storage.instance.getAllKeys();
+            const data = allKeys.filter(key => key.includes('id-'));
+            const user = await Storage.instance.get(data.toString());
+            return JSON.parse(user);
+        } catch (err) {
+            console.log('Get user err', err);
+        }
+    };
+
+    getToken = async username => {
+        try {
+            const key = `token-${username}`;
+            return await Storage.instance.get(key);
+        } catch (err) {
+            console.log('Get token error', err);
+        }
+    };
+
+    editProfile = async (id, token, body) => {
+        try {
+            let request = await fetch(`https://django-api-ex.herokuapp.com/profile/${id}/`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    Authorization: `Token ${token}`,
+                },
+            body: body,
+            });
+            let response = await request.json();
+            return response;
+        } catch (err) {
+            console.log('Edit profile error', err);
+        }
+    }
+
+}
+
+export default UserSession;
