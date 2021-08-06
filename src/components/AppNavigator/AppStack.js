d645002678c281304dfae66b57ff30bdc44f0324
david-trejo-armenta <a6518150041@utch.edu.mx>
2021-08-06 01:56:54 -0600

    App stack update

---
+++ src/components/AppNavigator/AppStack.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {createStackNavigator} from '@react-navigation/stack'
+import BadgesTabNavigator from '../BadgesScreen/BadgesTabNavigator'
+import BadgeLanding from '../BadgesLanding/BadgeLanding'
+import Colors from '../../res/Colors'
+
+const Stack = createStackNavigator();
+
+const AppStack = () => {
+    return(
+        <Stack.Navigator
+        screenOptions={{
+            headerShown: false,
+            headerStyle: {
+                backgroundColor: Colors.charade,
+                shadowColor: Colors.white,
+            },
+            headerTintColor: Colors.white,
+        }}>            
+            <Stack.Screen 
+            name = "Landing" 
+            component ={BadgeLanding} 
+            options ={{headerShown: false}}
+            />
+            <Stack.Screen 
+            name = "Badges" 
+            component = {BadgesTabNavigator} 
+            />
+          
+
+        </Stack.Navigator>
+    );
+}
+
+export default AppStack
